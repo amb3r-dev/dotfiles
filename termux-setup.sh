0c32c5f96aae87e7ce2d0336e904dd09c4255f8f
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-04-17 16:45:19 -0700

    Formatting + comments improvement & stored paths in variables.

--- termux-setup.sh
+++ termux-setup.sh
@@ -15,20 +15,28 @@ packages=(
 
 pkg install ${packages[@]}
 
+### Debian proot-distro setup
+debian_tarball_location=$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz
+debian_tmp_rootfs=$PREFIX/tmp/debian-rootfs
+
 read -p "Generate new Debian tarball for proot-distro? [y/N]: " answer
 [[ $answer =~ ^(Y|y)$ ]] && {
-  rm -r $PREFIX/tmp/debian-rootfs
-  rm $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz
-  debootstrap --arch=arm64  bookworm $PREFIX/tmp/debian-rootfs
-  tar czvf $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz -C $PREFIX/tmp/debian-rootfs $PREFIX/tmp/debian-rootfs/*
-  read -p "Enter your desired username to be used during setup: " username
-}
+  rm -r $debian_tmp_rootfs
+  rm $debian_tarball_location
 
-cat <<EOF > $PREFIX/etc/proot-distro/debian-${debian_suite}.sh
+  # Make the tarball
+  debootstrap --arch=arm64  bookworm $debian_tmp_rootfs
+  tar czvf $debian_tarball_location -C $debian_tmp_rootfs ${debian_tmp_rootfs}/*
+
+  # Get the default user's name
+  read -p "Enter your desired username to be used during setup: " username
+
+  # Create the proot-distro plugin script
+  cat << EOF > $PREFIX/etc/proot-distro/debian-${debian_suite}.sh
 
 DISTRO_NAME="Debian ${debian_suite^}"
-TARBALL_URL['aarch64']="file://$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz"
-TARBALL_SHA256['aarch64']="$(sha256sum $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz | awk '{ print $1 }')"
+TARBALL_URL['aarch64']="file://$debian_tarball_location"
+TARBALL_SHA256['aarch64']="$(sha256sum $debian_tarball_location | awk '{ print $1 }')"
 
 distro_setup() {
   run_proot_cmd adduser $username
@@ -37,3 +45,5 @@ distro_setup() {
 }
 
 EOF
+}
+
