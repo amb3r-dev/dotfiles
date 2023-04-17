a2c3015db67795da961a815caa3a5f1c6adb97f6
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-04-17 11:39:51 -0700

    Added experimental custom debian tarball bootstrapper. Included proot-distro plugin may not work.

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,7 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+debian_suite=bookworm
+
 packages=(
   proot-distro
+  debootstrap
   termux-api
   termux-exec
   termux-gui-bash
@@ -11,3 +14,21 @@ packages=(
 )
 
 pkg install ${packages[@]}
+
+debootstrap --make-tarball=$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz bookworm $PREFIX/tmp/debian-rootfs
+
+read -p "Enter your desired username to be used during setup: " username
+
+cat <<EOF > $PREFIX/etc/proot-distro/debian-${debian_suite}.sh
+
+DISTRO_NAME="Debian ${debian_suite^}"
+TARBALL_URL['aarch64']="file://$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz"
+TARBALL_SHA256['aarch64']="$(sha256sum $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz)"
+
+distro_setup() {
+  run_proot_cmd adduser $username
+  run_proot_cmd apt update
+  run_proot_cmd apt upgrade
+}
+
+EOF
