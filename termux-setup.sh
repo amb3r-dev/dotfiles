2d1d31de2920287f0fd3c19566d68ea9d356ed35
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-04-17 16:09:37 -0700

    Remove another incorrectly used flag.

--- termux-setup.sh
+++ termux-setup.sh
@@ -20,7 +20,7 @@ read -p "Generate new Debian tarball for proot-distro? [y/N]: " answer
   rm -r $PREFIX/tmp/debian-rootfs
   rm $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz
   debootstrap --arch=arm64  bookworm $PREFIX/tmp/debian-rootfs
-  tar czf $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz --directory=$PREFIX/tmp/debian-rootfs
+  tar czf $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz $PREFIX/tmp/debian-rootfs/*
   echo "Debian rootfs looks like this: "
   tar tfz $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz
   read -p "Enter your desired username to be used during setup: " username
