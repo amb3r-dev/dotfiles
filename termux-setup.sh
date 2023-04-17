f480246add4f8cff0d0cef943c79482780a0bbc8
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-04-17 12:00:02 -0700

    Add prompt before tarball generation.

--- termux-setup.sh
+++ termux-setup.sh
@@ -15,15 +15,19 @@ packages=(
 
 pkg install ${packages[@]}
 
-debootstrap --make-tarball=$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz bookworm $PREFIX/tmp/debian-rootfs
-
-read -p "Enter your desired username to be used during setup: " username
+read -p "Generate new Debian tarball for proot-distro? [y/N]" username
+[[ $answer =~ ^(Y|y)$ ]] && {
+  rm -r $PREFIX/tmp/debian-rootfs
+  rm $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz
+  debootstrap --make-tarball=$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz bookworm $PREFIX/tmp/debian-rootfs
+  read -p "Enter your desired username to be used during setup: " username
+}
 
 cat <<EOF > $PREFIX/etc/proot-distro/debian-${debian_suite}.sh
 
 DISTRO_NAME="Debian ${debian_suite^}"
 TARBALL_URL['aarch64']="file://$PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz"
-TARBALL_SHA256['aarch64']="$(sha256sum $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz)"
+TARBALL_SHA256['aarch64']="$(sha256sum $PREFIX/etc/proot-distro/debian-${debian_suite}-rootfs.tar.gz | awk '{ print $1 }')"
 
 distro_setup() {
   run_proot_cmd adduser $username
