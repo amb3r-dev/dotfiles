e3df820757ef653024f7d5a9d7670b6e4994da62
Amber Connelly <113668892+amb3r-dev@users.noreply.github.com>
2023-04-17 18:08:46 -0700

    Fixed user creation command.

--- termux-setup.sh
+++ termux-setup.sh
@@ -22,29 +22,26 @@ debian_tmp_rootfs=$PREFIX/tmp/debian-rootfs
 # Create the Debian tarball
 read -p "Generate new Debian ${debian_suite^} tarball for proot-distro? [y/N]: " answer
 [[ $answer =~ ^(Y|y)$ ]] && {
+  # Clear out the old stuff
   rm -r $debian_tmp_rootfs
   rm $debian_tarball_location
-
   # Make the tarball
   debootstrap --arch=arm64  bookworm $debian_tmp_rootfs
   cd $debian_tmp_rootfs && tar czvf $debian_tarball_location ./*
-
-  # Get the default user's name
-  read -p "Enter your desired username to be used during setup: " username
 }
 
 # Create the proot-distro plugin script
 read -p "Generate new Debian ${debian_suite^} script for proot-distro? [y/N]: " answer
 [[ $answer =~ ^(Y|y)$ ]] && {
+  # Get the default user's name
+  read -p "Enter your desired username to be used during setup: " username
   cat << EOF > $PREFIX/etc/proot-distro/debian-${debian_suite}.sh
 DISTRO_NAME="Debian ${debian_suite^}"
 TARBALL_URL['aarch64']="file://$debian_tarball_location"
 TARBALL_SHA256['aarch64']="$(sha256sum $debian_tarball_location | awk '{ print $1 }')"
 
 distro_setup() {
-  run_proot_cmd adduser $username
-  run_proot_cmd apt update
-  run_proot_cmd apt upgrade
+  run_proot_cmd useradd -m $username -G sudo
 }
 EOF
 }
